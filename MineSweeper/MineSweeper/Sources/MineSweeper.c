6e80dff29b39ac5ef3a9824304244cd7a6f840b9
Kunosato <cleciracsute@yahoo.co.jp>
2013-01-03 20:06:01 +0900

    SelectLevel関数

--- MineSweeper/MineSweeper/Sources/MineSweeper.c
+++ MineSweeper/MineSweeper/Sources/MineSweeper.c
@@ -1,7 +1,42 @@
 #include<stdio.h>
+#define EASY 1
+#define NORMAL 2
+#define HARD 3
+#define ROWS_OF_EASY 9
+#define LINES_OF_EASY 9
+#define ROWS_OF_NORMAL 15
+#define LINES_OF_NORMAL 15
+#define ROWS_OF_HARD 15
+#define LINES_OF_HARD 20
+
+void SetField(int *pLevel);
+void SelectLevel();
+
 
 int main(){
-	printf("test");
+
+	int level;
+	int *pLevel = &level;
+	struct Cell {
+		int isActive_;
+		int isMine_;
+		int state_;
+		int info_;
+	};
+	struct Cell cells[ROWS_OF_HARD][LINES_OF_HARD];
+
+	SelectLevel(pLevel);
 
 	return 0;
+}
+
+void SelectLevel(int *pLevel){
+	do{
+		printf("難易度を選んでください(初級：1 中級：2 上級：3)：");
+		scanf("%d", pLevel);
+	}while(*pLevel < EASY || *pLevel > HARD);
+}
+
+void Display(){
+
 }
